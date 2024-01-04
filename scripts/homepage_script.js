af0cebb0f59827c429d5f51fb33b9e74a739ee99
Alex <alex.turano589@gmail.com>
2024-01-04 22:16:18 +0800

    Added responsiveness to code

--- scripts/homepage_script.js
+++ scripts/homepage_script.js
@@ -2,7 +2,7 @@ var root = document.querySelector(':root');
 window.addEventListener("scroll", (event) => {
     let scroll = this.scrollY;
     if (scroll > 50){
-        root.style.setProperty('--navigation-color', '#f4f4f4')
+        root.style.setProperty('--navigation-color', 'var(--bs-white)')
     }
     else{
         root.style.setProperty('--navigation-color', 'rgba(0, 0, 0, 0.2)')
