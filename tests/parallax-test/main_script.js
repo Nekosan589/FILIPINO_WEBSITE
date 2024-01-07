b5b99aa3da96d67febb3521aefb76ce8fe45c357
Alex <alex.turano589@gmail.com>
2024-01-07 22:02:25 +0800

    Parralasx

--- tests/parallax-test/main_script.js
+++ tests/parallax-test/main_script.js
@@ -1,7 +1,17 @@
 let text = document.getElementById("text");
+let greenBox = document.getElementById("green-box");
 
 window.addEventListener("scroll", () => {
     let value = window.scrollY;
-    text.style.marginTop = value * 2.5 + 'px';
+    if (value < 500){
+        text.style.opacity = (500 - value)/500;
+        console.log(text.style.opacity);
+    }
+    else{
+        text.style.opacity = 0;
+        console.log(text.style.opacity);
+    }
+    greenBox.style.top = value * 1.5 + 'px';
+    greenBox.style.right = value * -0.5 + 'px';
     
 });
